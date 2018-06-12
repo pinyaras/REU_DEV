27ae2ddaf2dedcd026c1567befb795abb261adf0
Dawson Heide <dheide@uncc.com>
2018-06-12 15:24:21 -0400

    started working on test-d3 component

--- src/app/layout/test-d3/test-d3.component.ts
+++ src/app/layout/test-d3/test-d3.component.ts
@@ -2,11 +2,13 @@ import { Component, ElementRef, NgZone, OnDestroy, OnInit } from '@angular/core'
 import { Node } from '../../shared/node';
 import { Link } from '../../shared/link';
 import { Network } from '../../shared/network';
-
+import { NetworkService } from '../../shared/services/network.service'
 import {
   D3Service,
   D3
 } from 'd3-ng2-service';
+
+
 
 @Component({
   selector: 'app-test-d3',
@@ -22,18 +24,22 @@ export class TestD3Component implements OnInit {
         "switch": "dodgerblue"
     };
     private static readonly SVG_FILL = "LightSlateGray";
+    private network: Network;
 
-  private d3: D3;
-  private parentNativeElement: any;
+    private d3: D3;
+    private parentNativeElement: any;
 
-  constructor(d3Service: D3Service) {
-    this.d3 = d3Service.getD3();
+    constructor(d3Service: D3Service, networkService: NetworkService) {
+      this.d3 = d3Service.getD3();
+      networkService.getNetwork().subscribe(data => this.network = data);
+    }
 
-  }
-
-  ngOnInit() {
+    ngOnInit() {
+        
         let svg = this.d3.select("svg")
         svg.style("background-color", TestD3Component.SVG_FILL);
 
-   }
+        console.log(this.network);
+
+    }
 }
