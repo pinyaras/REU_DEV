c5ebac1c1f32bed8b4d8b816986c3676d37e54d2
kerwin mercado <kerwin.mercado@uconn.edu>
2018-06-13 10:25:34 -0400

    node border (commented out)

--- src/app/layout/test-d3/test-d3.component.ts
+++ src/app/layout/test-d3/test-d3.component.ts
@@ -45,13 +45,13 @@ export class TestD3Component implements OnInit {
 
         let delete_hover = function(){
           svg.select("#hover").remove();
-          d3.select(this).attr('r', 20);
+          d3.select(this).attr('r', TestD3Component.NODE_RADIUS);
         }
 
         let on_hover = function(d){
           svg.select("#hover").remove();
           let coords = d3.mouse(this);
-          d3.select(this).attr('r', 25);
+          d3.select(this).attr('r', TestD3Component.NODE_RADIUS + 5);
           // console.log(coords);
           let g = svg.append("g")
             .attr("id", "hover");
@@ -98,6 +98,8 @@ export class TestD3Component implements OnInit {
           .enter()
           .append("circle")
           .attr("r", TestD3Component.NODE_RADIUS)
+         // .attr('stroke', "white")
+         // .attr('stroke-width', 2)
           .attr("fill", function(node){
             return TestD3Component.COLORS[node.type]
           })
