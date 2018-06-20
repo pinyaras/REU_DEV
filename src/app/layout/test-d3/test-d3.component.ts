167ec2f15eadf4161d3fb4c2f1a2cc9eb1193ef9
Mark Powers <markppowers0@gmail.com>
2018-06-20 09:35:56 -0400

    Fix timer to avoid recursion

--- src/app/layout/test-d3/test-d3.component.ts
+++ src/app/layout/test-d3/test-d3.component.ts
@@ -18,6 +18,7 @@ export class TestD3Component {
   private old_links: Link[];
   constructor(private networkService: NetworkService) {
     this.load();
+    setTimeout(() => this.load(), 6000);
   }
   private array_equal(a1, a2): boolean {
     if (a1 && a2) {
@@ -68,7 +69,5 @@ export class TestD3Component {
       }
       comp.old_links = link_data;
     });
-
-    setTimeout(() => this.load(), 6000);
   }
 }
