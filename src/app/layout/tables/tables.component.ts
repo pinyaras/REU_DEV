888097424b552cc65e402e081ee8d7c8b3379c56
Mark Powers <markppowers0@gmail.com>
2018-06-21 15:46:32 -0400

    Add chart for flow stats

--- src/app/layout/tables/tables.component.ts
+++ src/app/layout/tables/tables.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
 import { ControllerStatsticsService } from '../../shared/services/controller-statstics.service'
 
-import {FlowStats} from '../../shared/flow-stats'
+import { FlowStats } from '../../shared/flow-stats'
 import { SwitchFlowStats } from '../../shared/switch-flow-stats';
 import { SwitchPortStats } from '../../shared/switch-port-stats';
 import { PortStats } from '../../shared/port-stats';
+import { filter } from 'rxjs/operator/filter';
+import { ChartsModule as Ng2Charts } from 'ng2-charts';
 
 @Component({
     selector: 'app-tables',
@@ -13,17 +15,101 @@ import { PortStats } from '../../shared/port-stats';
 })
 export class TablesComponent implements OnInit {
     private switches: number[];
+
     private flowStats: SwitchFlowStats[];
     private portStats: SwitchPortStats[];
+    private old_data;
+    private labels;
 
     constructor(private controllerStatsticsService: ControllerStatsticsService) {
         this.flowStats = [];
         this.portStats = [];
         this.switches = [];
+        this.old_data = [];
+        this.labels = [];
         this.getFlowStats();
         this.getPortStats();
     }
+
+    barChartData(): any[] {
+        if (!this.flowStats) {
+            return []
+        }
+        var flowStatsList = this.flowStats.map(function (element) {
+            return element.stats
+        });
+        var data = [];
+        var index = 0;
+        var max_length = 0;
+        for (let i = 0; i < flowStatsList.length; i++) {
+            if (flowStatsList[i].length > max_length) {
+                max_length = flowStatsList[i].length;
+            }
+        }
+        for (let i = 0; i < max_length; i++) {
+            data.push(
+                {
+                    "data": [],
+                    "label": i
+                }
+            )
+        }
+        for (let i = 0; i < max_length; i++) {
+            var list = data[i].data;
+            for (let j = 0; j < flowStatsList.length; j++) {
+                if (flowStatsList[j][i]) {
+                    list.push(flowStatsList[j][i].packet_count)
+                } else {
+                    list.push(0);
+                }
+            }
+        }
+
+        if (this.dataChanged(data, this.old_data)) {
+            this.old_data = data;
+        }
+        return this.old_data;
+    }
+    dataChanged(data, old_data): boolean {
+        if (data.length != old_data.length) {
+            return true;
+        } else {
+            for (var index = 0; index < data.length; index++) {
+                if (data[index].data.length != old_data[index].data.length
+                    || data[index].length != old_data[index].length) {
+                    return true;
+                } else {
+                    for (var index2 = 0; index2 < data.length; index2++) {
+                        if (data[index].data[index2] != old_data[index].data[index2]) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    barChartLabels(): string[] {
+        if (!this.flowStats) {
+            return []
+        }
+        var labels = this.flowStats.map(function (element) {
+            return "Switch " + element.id;
+        });
+        if (this.labels.length != labels.length) {
+            // console.log(labels);
+            this.labels = labels;
+        }
+        for (var i = 0; i < this.labels.length; i++) {
+            if (this.labels[i] != (labels[i])) {
+                this.labels = labels;
+                return this.labels;
+            }
+        }
+        return this.labels;
+    }
     ngOnInit() { }
+
     getSwitches(after = null) {
         var comp = this;
         this.controllerStatsticsService.getSwitches().subscribe((data) => {
--- src/app/layout/tables/tables.module.ts
+++ src/app/layout/tables/tables.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ChartsModule as Ng2Charts } from 'ng2-charts';
 import { TablesComponent } from './tables.component';
 import { TablesRoutingModule } from './tables-routing.module';
 import { PageHeaderModule } from './../../shared';
@@ -9,7 +10,8 @@ import { PageHeaderModule } from './../../shared';
     imports: [
         CommonModule,
         TablesRoutingModule,
-        PageHeaderModule
+        PageHeaderModule,
+        Ng2Charts
     ],
     declarations: [TablesComponent]
 })
